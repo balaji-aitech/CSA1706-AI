0d909123c93f8bcdff82a4b0cc948ea5bac9c55f
balajibalaji15052008-boop <balaji.balaji15052008@gmail.com>
2026-07-28 21:16:53 +0530

    Create exp 7 ai.py

---
+++ Python/exp 7 ai.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': [],
+    'F': []
+}
+
+visited = set()
+queue = deque(['A'])
+
+print("BFS Traversal:")
+
+while queue:
+    node = queue.popleft()
+
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+
+        for neighbour in graph[node]:
+            queue.append(neighbour)
